9b9c0d90338260e90d43fe2f011e96a2430ca775
jesse <jsoldat@hotmail.com>
2017-06-01 08:19:34 -0400

    started working on login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule, Routes } from '@angular/router';
+import { environment } from '../environments/environment';
+//Components
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { LoginFormComponent } from './components/shared/login-form/login-form.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+//Services
+//Third Party Modules
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { Angular2FontAwesomeModule } from 'angular2-font-awesome/angular2-font-awesome';
+
+const appRoutes: Routes = [
+  // {path: '', component: AppComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'login', component: LoginComponent}
+  
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LoginFormComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    Angular2FontAwesomeModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
